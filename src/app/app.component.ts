76467e06bc9b0fc1eac3009d8253c6d8c6e68286
Aymeric Giraudet <aymeric.giraudet@etudiant.univ-lille1.fr>
2017-10-20 23:05:12 +0200

    Downloads a ubuntu iso with a progressBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Transmission, SettingsConfig } from "node-transmission-typescript";
+import { IAddTorrent } from "node-transmission-typescript/dist/models";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,27 @@ import { Transmission, SettingsConfig } from "node-transmission-typescript";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PFE';
+  value = 0;
+
+  private get(transmission: Transmission, result: IAddTorrent): void {
+    var prom2 = transmission.get([result.id]);
+    prom2.then((res) => {
+      this.value = Math.floor((res[0].downloadedEver / res[0].sizeWhenDone) * 100);
+      if(res[0].downloadedEver >= res[0].sizeWhenDone){ return; }
+      setTimeout(this.get(transmission, result), 1000);
+    });
+  }
+
+  public onClick(): void {
+    var settings = 
+    new SettingsConfig({ host: "cors-anywhere.herokuapp.com/aymericard.me", port: 9091,
+    username: "transmission", password: "amaguiz"
+    });
+    var transmission = new Transmission(settings);
+    var prom = transmission.addUrl('http://releases.ubuntu.com/14.04.1/ubuntu-14.04.1-desktop-amd64.iso.torrent');
+    prom.then((res) => {
+        this.get(transmission, res);
+    });
+  }
 }
